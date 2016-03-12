68c1c2fc4cfb2206b1be3344264eed2192784f22
CattaO <cata_oaida@yahoo.com>
2016-03-12 18:08:23 +0200

    color to sound

--- Spykes/CloudVision/app/src/main/java/inovation/lab/cloudvision/MainActivity.java
+++ Spykes/CloudVision/app/src/main/java/inovation/lab/cloudvision/MainActivity.java
@@ -19,6 +19,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 import  android.graphics.Color;
+import android.content.Context;
 
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
@@ -41,8 +42,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 import android.media.MediaPlayer;
+import android.media.AudioManager;
 
 import sample.google.com.cloudvision.R;
+
 
 public class MainActivity extends AppCompatActivity {
     private static final String CLOUD_VISION_API_KEY = "AIzaSyBoyen2uL0vc4sf8viYAI78cjHwrgqrmWg";
@@ -154,15 +157,7 @@ public class MainActivity extends AppCompatActivity {
                         Log.d("X & Y", values[0] + " " + values[1]);
 
                         int touchedRGB = bitmap.getPixel(values[0], values[1]);
-
-                        int red = Color.red(touchedRGB);
-                        int blue = Color.blue(touchedRGB);
-                        int green = Color.green(touchedRGB);
-
-                        Log.d("R", Integer.toString(red));
-                        Log.d("G", Integer.toString(green));
-                        Log.d("B", Integer.toString(blue));
-
+                        ColorToSOund(touchedRGB);
                     }
                 });
 
@@ -280,5 +275,74 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return message;
+    }
+
+    private void ColorToSOund(int color){
+        int red = Color.red(color);
+        int blue = Color.blue(color);
+        int green = Color.green(color);
+
+        Log.d("R", Integer.toString(red));
+        Log.d("G", Integer.toString(green));
+        Log.d("B", Integer.toString(blue));
+
+        if(blue == 255 && red == 255 && green == 255){
+            // play white
+            PlayColor("white", 1);
+        }else if(blue == 0 && red == 0 && green == 0){
+            // play black
+            PlayColor("black", 1);
+        }else{
+            if(blue > 128 && red > 128 && green > 128){
+                // play white half volume
+                PlayColor("white", 0.5f);
+            }
+
+            if(blue < 128 && red < 128 && green < 128){
+                // play black half volume
+                PlayColor("black", 0.5f);
+            }
+
+            // set volume color/255
+            // play red
+            PlayColor("red", (float)red / 255);
+            // play green
+            PlayColor("green", (float)green / 255);
+            // play blue
+            PlayColor("blue", (float)blue / 255);
+        }
+    }
+
+    private void PlayColor(String color, float volume){
+        Uri myUri = null;
+
+        switch (color){
+            case "red":
+                myUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.red); // initialize Uri here
+                break;
+            case "green":
+                myUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.green); // initialize Uri here
+                break;
+            case "blue":
+                myUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.blue); // initialize Uri here
+                break;
+            case "white":
+                myUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.white); // initialize Uri here
+                break;
+            case "black":
+                myUri = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.black); // initialize Uri here
+                break;
+        }
+
+        try {
+            MediaPlayer mediaPlayer = new MediaPlayer();
+            mediaPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC);
+            mediaPlayer.setDataSource(getApplicationContext(), myUri);
+            mediaPlayer.setVolume(volume,volume);
+            mediaPlayer.prepare();
+            mediaPlayer.start();
+        } catch (Exception e) {
+            Log.d(TAG, "BUBA!!!");
+        }
     }
 }
